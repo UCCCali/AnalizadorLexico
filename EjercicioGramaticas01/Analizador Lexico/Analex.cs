7955b852dbb26388dfcb95690b5feaf9fb76f66e
jggomez <juan.gomez01@gmail.com>
2015-10-02 18:00:48 -0500

    Se sube primera version

---
+++ EjercicioGramaticas01/Analizador Lexico/Analex.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EjercicioGramaticas01.Analizador_Lexico
+{
+    public class Analex
+    {
+
+        int numLinea = 1;
+        String valcomplex;
+        String[] reservadas = { "if", "while", "do", "for", "class", "else", "true", "double", "final", "String", "int"};
+        List<String> lstPalabrasReservadas = new List<string>();
+
+        public void AnalizadorLexico(String nombreArchivo)
+        {
+            char car;
+            int val;
+            StreamReader file = new StreamReader(nombreArchivo);
+
+            foreach (var item in reservadas)
+            {
+                lstPalabrasReservadas.Add(item);
+            }
+
+            while (true)
+            {
+                val = file.Read();
+
+                if (val == -1)
+                {
+                    break;
+                }
+
+                valcomplex = "";
+                car = (char)val;
+
+                //Eliminación de espacios en blanco
+                while (car == ' ' || car == '\n' || car == '\r' || car == '\t')
+                {
+                    if (car == '\n')
+                    {
+                        numLinea++;
+                    }
+                    val = file.Read();
+                    car = (char)val;
+                }
+
+                //Reconocimiento de numeros
+                if (Char.IsNumber(car))
+                {
+                    valcomplex = valcomplex + car;
+
+                    while (true)
+                    {
+                        val = file.Peek();
+                        car = (char)val;
+                        if (Char.IsNumber(car)) { valcomplex = valcomplex + car; file.Read(); }
+                        else { break; }
+                    }
+
+                    if (car == '.')
+                    {
+                        valcomplex = valcomplex + car;
+                        file.Read();
+
+                        while (true)
+                        {
+                            val = file.Peek();
+                            car = (char)val;
+                            if (Char.IsNumber(car)) { valcomplex = valcomplex + car; file.Read(); }
+                            else { break; }
+                        }
+                    }
+
+                    Mostrar("NUMERO");                    
+                    continue;
+
+                }
+
+
+                //Reconocimiento de identificadores y palabras reservadas
+                if (Char.IsLetter(car))
+                {
+                    valcomplex = valcomplex + car;                    
+
+                    while (true)
+                    {
+                        val = file.Peek();
+                        car = (char)val;
+                        if (Char.IsNumber(car) || Char.IsLetter(car) || car == '$' || car == '_') { valcomplex = valcomplex + car; file.Read(); }
+                        else { break; }
+                    }
+
+                    if (lstPalabrasReservadas.Contains(valcomplex))
+                    {
+                        Mostrar("RESERVADA");
+                    }
+                    else
+                    {
+                        Mostrar("ID");
+                    }
+                    
+                    continue;
+                }
+
+
+                //Reconocimiento de cadenas
+                if (car == '"')
+                {
+                    while (true)
+                    {
+                        val = file.Read();
+                        car = (char)val;
+                        if (car != '"') { valcomplex = valcomplex + car;}
+                        else { break; }
+                    }
+
+                    Mostrar("CADENA");
+                    
+                    continue;
+                }
+
+
+                //Reconocimiento de comentarios
+                if (car == '/')
+                {
+                    valcomplex = valcomplex + car;                    
+                    val = file.Peek();
+                    car = (char)val;
+
+                    if (car == '/')
+                    {
+                       valcomplex = valcomplex + car;
+                       file.Read();
+
+                       while (true)
+                       {
+                            val = file.Peek();
+                            car = (char)val;
+                            if (car != '\n' && car != '\r') { valcomplex = valcomplex + car; file.Read(); }
+                            else { break; }
+                        }
+
+                        Mostrar("COMENTARIO");
+                    }
+                    else if (car == '*')
+                    {
+                        valcomplex = valcomplex + car;
+                        file.Read();
+
+                        while (true)
+                        {
+                            val = file.Read();
+                            car = (char)val;
+                            valcomplex = valcomplex + car;
+
+                            if (car == '*')
+                            {
+                                val = file.Read();
+                                car = (char)val;
+                                valcomplex = valcomplex + car;
+                                if (car == '/') break;
+                            }
+                        }
+
+                        Mostrar("COMENTARIO");
+                        continue;
+                    }                  
+                }
+               
+                // Reconocimiento de operadores
+                valcomplex = valcomplex + car;
+                switch (car)
+                {
+                    case '\r': break;
+                    case '+':
+                    case '.':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case ';': Mostrar("OPERADOR"); break;
+                    default: Mostrar("DESCONOCIDO"); break;
+                }                 
+                 
+            }
+        }
+
+        private void Mostrar(String msg)
+        {
+            Console.WriteLine(numLinea + ": " + msg + "(" + valcomplex + ")");
+        }
+    }
+}
---
+++ EjercicioGramaticas01/Program.cs
@@ -0,0 +1,20 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioGramaticas01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Analizador_Lexico.Analex objAnalex = new Analizador_Lexico.Analex();
+
+            objAnalex.AnalizadorLexico(@"C:\log\Ejemplo1.txt");
+
+            Console.ReadLine();
+        }
+    }
+}
